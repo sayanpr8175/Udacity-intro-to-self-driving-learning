e11cb99189925ecd81533c86c3393d507fcc2b34
sayan8175 <sayan.pramanik8175@gmail.com>
2022-04-21 00:56:33 +0530

    predict func added

--- Kalman Filters/UpdateNewMeanAndVariance.py
+++ Kalman Filters/UpdateNewMeanAndVariance.py
@@ -14,3 +14,11 @@ def update(mean1, var1, mean2, var2):
 print (update(10.,4.,12., 4.))
 
 
+## Gaussian Motion / motion predict
+
+def predict(mean1, var1, mean2, var2):
+    new_mean = mean1 + mean2
+    new_var = var1 + var2
+    return [new_mean, new_var]
+
+
